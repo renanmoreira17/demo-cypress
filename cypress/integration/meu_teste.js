2dbed2caf73a17511e374e311dc4bc7e653a8a3e
Renan Moreira <renanmoreira17@gmail.com>
2022-02-05 16:39:08 -0300

    Teste de sistema Cypress

---
+++ cypress/integration/meu_teste.js
@@ -0,0 +1,12 @@
+// meu_teste.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Meu primeiro teste', () => {
+    it('Não faz nada', () => {
+        expect(true).to.equal(true)
+    })
+})
